c5ffd46fffec9bbc55fcf76bc90d4f4d4ba71e88
Ryo Nakae <ryo.n.1129@gmail.com>
2019-02-11 03:37:30 +0900

    update stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,9 +1,33 @@
+const css = {
+  properties: 'assets/styles/properties.css',
+  media: 'assets/styles/media.css'
+}
+
 module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-recess-order'],
+  plugins: [
+    'stylelint-value-no-exposed-custom-properties',
+    'stylelint-value-no-unknown-custom-properties',
+    'stylelint-media-use-custom-media',
+    'stylelint-use-nesting'
+  ],
   rules: {
     'declaration-colon-newline-after': null,
     'value-list-comma-newline-after': null,
     'no-descending-specificity': null,
-    'selector-pseudo-class-no-unknown': null
+    'selector-pseudo-class-no-unknown': null,
+    'plugin/value-no-exposed-custom-properties': [
+      true,
+      { importFrom: [css.properties] }
+    ],
+    'csstools/value-no-unknown-custom-properties': [
+      true,
+      { importFrom: [css.properties] }
+    ],
+    'csstools/media-use-custom-media': [
+      'always-known',
+      { importFrom: [css.media] }
+    ],
+    'csstools/use-nesting': 'always'
   }
 }
